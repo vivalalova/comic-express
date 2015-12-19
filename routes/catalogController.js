4e716230c6a663ab056af9e37b66fe990ae0df11
vivalalova <vivalalova0@gmail.com>
2015-12-20 05:37:08 +0800

    modify parameters

--- routes/catalogController.js
+++ routes/catalogController.js
@@ -7,10 +7,8 @@ var router = express.Router();
 var chapter = require('./catalog/chapterController.js');
 var page = require('./catalog/pageController.js');
 
-router.get('/', function(req, res, next) {
-    console.log(req.params);
 
-
+function parseParams(req) {
     var dict = {};
 
     if (req.param('title')) {
@@ -25,23 +23,39 @@ router.get('/', function(req, res, next) {
         };
     }
 
+    return dict;
+}
+
+function parseLimit(req) {
+    var limit = req.param('limit') ? req.param('limit') : 30;
+    return limit > 100 ? 100 : limit;
+}
+
+function parseSkip(req) {
+    return req.param('skip') ? req.param('skip') : 0;
+}
+
+router.get('/', function(req, res, next) {
 
     var limit = req.param('limit') ? req.param('limit') : 30;
     limit = limit > 100 ? 100 : limit;
 
 
     DB.Catalog.find(
-        dict
-    ).skip(req.param('skip') ? req.param('skip') : 0).limit(limit).exec(function(err, data) {
+        parseParams(req)
+    ).
+    skip(parseSkip(req)).
+    limit(parseLimit(req)).
+    exec(function(err, data) {
         if (err) return res.send(err);
         res.send(data);
     });
 });
 
 router.get('/:id', function(req, res, next) {
+    console.log(req.param('id'));
     DB.Catalog.find({
-        where: {},
-        limit: req.params.limit
+        "_id": req.param('id')
     }, function(err, data) {
         if (err) return res.badRequest(err);
         res.send(data);
